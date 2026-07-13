ad018189068b5d9811999135401e1da0ce87aaef
Nishant Giri <150437678+Nishant5261@users.noreply.github.com>
2026-07-13 21:55:53 +0530

    Added solution - LeetHub

---
+++ 13499-eye-and-identity-Hackerrank/13499-eye-and-identity.py
@@ -0,0 +1,14 @@
+import numpy
+numpy.set_printoptions(legacy='1.13')
+
+N, M = map(int, input().split())
+
+print(numpy.eye(N,M))
+
+
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
