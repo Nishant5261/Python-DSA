c58401e8a43ffd757462b5e3b47732c64515dd71
Nishant Giri <150437678+Nishant5261@users.noreply.github.com>
2026-07-13 20:22:18 +0530

    Time: 8 ms (71.54%), Space: 12.3 MB (54.23%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        y=x
+        st=str(y)
+        return st==st[::-1]
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
