bbc8d9ace0120b8b0ef8fe055d6843b9533d5cd2
Nishant Giri <150437678+Nishant5261@users.noreply.github.com>
2026-07-17 14:45:10 +0530

    Time: 4 ms (100%), Space: 12.7 MB (66.47%) - LeetHub - Time: O(n), Space: O(n)

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.py
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.py
@@ -1,21 +1,22 @@
-class Solution(object):
-    def countSymmetricIntegers(self, low, high):
-        """
-        :type low: int
-        :type high: int
-        :rtype: int
-        """
-        c=0
-        for i in range(low,high+1):
-            n=str(i)
-            z=list(map(int,n))
-            l=len(z)
-            if l%2!=0:
-                continue
-            if sum(z[:l//2])==sum(z[l//2:]):
-                c+=1
-        return c
-        
+from array import array
+
+m = array('H', [0]) * 10001
+count = 0
+for i in range(11, 10001):
+    if i < 100 and i % 11 == 0:
+        count += 1
+    elif i > 1000:
+        i0 = i % 10
+        i1 = (i % 100) // 10
+        i2 = (i % 1000) // 100
+        i3 = i // 1000
+        if i0 + i1 == i2 + i3:
+            count += 1
+    m[i] = count
+
+class Solution:
+    def countSymmetricIntegers(self, l, h):
+        return m[h] - m[l - 1]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
