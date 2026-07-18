7a4a1cdb8a370a19c7d05c9ebcf7596e3e2844a2
Nishant Giri <150437678+Nishant5261@users.noreply.github.com>
2026-07-18 14:24:08 +0530

    Time: 1 ms (33.37%), Space: 12.5 MB (21.08%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
@@ -0,0 +1,22 @@
+
+def gcd_euclidean(a, b):
+    a, b = abs(a), abs(b)  
+    while b != 0:
+        a, b = b, a % b
+    return a
+class Solution(object):
+    def findGCD(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        s=nums[0]
+        l=nums[-1]
+        return gcd_euclidean(s,l)
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
